44b62a375724aa9bc72ff00065a7480878bed617
40iq <maksPTD6@mail.ru>
2023-08-04 20:54:15 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let sliceN = Math.abs(n);
+    for (let i = 0; i < sliceN.length; i++) {
+        result = `${sliceN[i]}${result}`;
+    }
+    return result;
 }
+
+console.log(reverse(2281));
+
+
